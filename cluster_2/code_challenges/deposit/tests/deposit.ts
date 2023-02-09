b0ab83a9cd7d697dbba3e2ec8f16db203a9d2d0c
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-02-09 17:21:27 -0300

    creating pda using a name as a seed

--- cluster_2/code_challenges/deposit/tests/deposit.ts
+++ cluster_2/code_challenges/deposit/tests/deposit.ts
@@ -1,16 +1,27 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Deposit } from "../target/types/deposit";
+import * as web3 from "@solana/web3.js"
 
 describe("deposit", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const anchorProvider = anchor.AnchorProvider.env();
+  anchor.setProvider(anchorProvider);
   const program = anchor.workspace.Deposit as Program<Deposit>;
+  const name = "test";
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Init a deposit", async () => {
+    const [vault] = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(name)],
+      program.programId,
+    )
+    const tx = await program.methods.initialize(name)
+    .accounts({
+      vault,
+      owner: anchorProvider.wallet.publicKey,
+      systemProgram: web3.SystemProgram.programId,
+    })
+    .rpc();
     console.log("Your transaction signature", tx);
   });
 });
